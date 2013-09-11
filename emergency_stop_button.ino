df02f07a05fa41790e26b97a6e906b03378a3dd7
Samuel Kadolph <samuel@kadolph.com>
2013-09-11 00:59:55 -0400

    Add reading serial port for READ command

--- emergency_stop_button.ino
+++ emergency_stop_button.ino
@@ -1,16 +1,30 @@
 const int button = 3;
 const long debounce = 50;
 
+char b;
+int previousState = 0;
+int state = 0;
 long lastBounce = 0;
 long lastSent = 0;
-int state = 0;
-int previousState = 0;
+String buffer;
 
 void setup() {
   pinMode(button, INPUT);
 }
 
 void loop() {
+  while (Serial.available()) {
+    b = Serial.read();
+
+    buffer += b;
+
+    if (b == '\n') {
+      if (buffer == "READ\n") lastSent = 0;
+
+      buffer = "";
+    }
+  }
+
   state = digitalRead(button);
 
   if (state != previousState) {
